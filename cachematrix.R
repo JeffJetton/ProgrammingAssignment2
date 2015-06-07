982b50eae9831f67f1fd06043251ad5adde82f85
Jeff Jetton <jeff.jetton@gmail.com>
2015-06-07 16:56:26 -0500

    Initial edits to template

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,63 @@
-## Put comments here that give an overall description of what your
-## functions do
+# These two functions take advantage of the <<- operator to
+# cache a costly operation (in this case, inverting a matrix)
 
-## Write a short comment describing this function
-
-makeCacheMatrix <- function(x = matrix()) {
-
+# Create a list containing a matrix along with
+# a potentially cached inverse of that matrix
+makeCacheMatrix <- function(current.matrix = matrix()) {
+	
+	# Start with an emply cache
+    cached.inverse <- NULL
+    
+    set <- function(new.matrix) {
+    	# Make the new matrix the current one
+    	# and clear out the cache
+        current.matrix <<- new.matrix
+        cached.inverse <<- NULL
+    }
+    
+    get <- function() {
+        # Simply return the current matrix
+        current.matrix
+    }
+    
+    setinverse <- function(new.inverse) {
+        # Store the passed matrix (presumably the
+        # inverse of the current matrix) in the cache
+        cached.inverse <<- new.inverse
+    }
+    
+    getinverse <- function() {
+        # Return whatever is in the cache...
+        cached.inverse
+    }
+    
+    list(set = set,
+         get = get,
+         setinverse = setinverse,
+         getinverse = getinverse)
 }
 
 
-## Write a short comment describing this function
+# Return the inverse of a matrix created with
+# makeCacheMatrix. If it has already been
+# calculated, just return cached value.
+cacheSolve <- function(x) {
 
-cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+   # First check the cache
+   inv <- x$getinverse()
+
+   # If there's no matrix in the cache,
+   # calculate it now...
+   if(is.null(inv)) {
+        inv <- solve(x$get())
+        # Be sure to cache the result
+        x$setinverse(inv)
+    } else {
+        # No need to recalculate
+        # Let the user know...
+        message("getting cached data")
+    }
+
+	# Return the inverse
+	inv
 }
